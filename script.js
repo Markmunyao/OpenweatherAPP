2e2be269bb53a1abe0817e5a3be79d3e2978199d
Mark Kusu <159567933+Markmunyao@users.noreply.github.com>
2024-05-01 03:53:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "a1379b8291b34bc81d35631481c9b26d";
+const apiKey = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const locationInput = document.getElementById("locationInput");
